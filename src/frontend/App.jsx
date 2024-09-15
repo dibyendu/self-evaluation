f7cb6a677eef4474772d795c6dc878d8f20ba161
Dibyendu Das <dibyendu.das.in@gmail.com>
2024-09-15 09:20:05 -0400

    Update App.jsx
    Signed-off-by: Dibyendu Das <dibyendu.das.in@gmail.com>

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -414,7 +414,7 @@ function App() {
                   return fetch(`${backEndHost}/start`, {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ joint_config: initial_joint_config, wait_time: waitTime, object_location: objectPose, task: currentTask })
+                    body: JSON.stringify({ joint_config: initial_joint_config, wait_time: waitTime, object_location: objectPose, task: currentTask === 'pouring' ? 'pour' : 'scoop' })
                   })
                   .then(response => {
                     calculateTimeElapsed('demonstration-preparation-time')
